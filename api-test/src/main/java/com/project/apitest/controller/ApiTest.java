fa34c27f2a3bcf5258bd5ea6787ab4b41856e473
LimbergVC <limbervillcacoraite@gmail.com>
2023-04-20 19:18:28 -0400

    get api hello-world

--- api-test/src/main/java/com/project/apitest/controller/ApiTest.java
+++ api-test/src/main/java/com/project/apitest/controller/ApiTest.java
@@ -1,7 +1,5 @@
 package com.project.apitest.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
